4ad18cd9a68d512c2ecd18d8636934a333c70f9b
skid <skid@kinkpad.local>
2025-04-04 09:03:54 +0200

    fix bubble sort

--- cpp/array/bubble_sort.cpp
+++ cpp/array/bubble_sort.cpp
@@ -3,8 +3,10 @@
 using namespace std;
 
 void bubble_sort(int v[], int len) {
-    for (int n = len-1; n > 0; n--) {
-        for (int i = 0; i < n; i++) {
+    for (int fine = len-1; fine > 0; fine--) {
+        for (int i = 0; i < fine; i++) {
+            // scorro l'array a coppie di elementi contigui e, se trovo
+            // che il primo e' maggiore del secondo, li scambio
             if (v[i] > v[i+1]) {
                 int temp = v[i];
                 v[i] = v[i+1];
@@ -16,18 +18,19 @@ void bubble_sort(int v[], int len) {
 
 int main() {
 
+    int dim = 5;
     int v[] = { 7, 1, 8, 4, 2 };
 
     cout << "PRIMA: ";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < dim; i++) {
         cout << v[i] << " ";
     }
     cout << endl;
 
-    bubble_sort(v, 5);
+    bubble_sort(v, dim);
 
     cout << "DOPO: ";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < dim; i++) {
         cout << v[i] << " ";
     }
     cout << endl;
